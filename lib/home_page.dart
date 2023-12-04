0d5751b1f19bd37cc8119615e8046ca7e7a57e92
Pregum <pregum.y@gmail.com>
2023-12-05 05:48:25 +0900

    wip: レイアウト設定

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,30 +12,77 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Supabase Flutter'),
       ),
-      body: Center(
-        child: FutureBuilder<List<Menu>>(
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator.adaptive();
-            }
+      body: Column(
+        children: [
+          Container(
+            height: 50,
+            color: Colors.red,
+            child: const Row(
+              children: [
+                Expanded(child: Center(child: Text('menu'))),
+                SizedBox(
+                  width: 8,
+                  child: VerticalDivider(
+                    color: Colors.grey,
+                    thickness: 4,
+                  ),
+                ),
+                Expanded(child: Center(child: Text('allergen'))),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.5,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: FutureBuilder<List<Menu>>(
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const CircularProgressIndicator.adaptive();
+                        }
 
-            final menus = snapshot.data;
-            if (menus == null) {
-              return const Text('No menus found');
-            }
-            return _buildMenuList(menus);
-          },
-          future: getMenus(),
-        ),
+                        final menus = snapshot.data;
+                        if (menus == null) {
+                          return const Center(child: Text('No menus found'));
+                        }
+                        return _buildMenuList(menus);
+                      },
+                      future: getMenus(),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                  child: VerticalDivider(
+                    color: Colors.grey,
+                    thickness: 4,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
   _buildMenuList(List<Menu> menus) {
     return ListView.builder(
+      // shrinkWrap: true,
       itemBuilder: (context, index) {
         final menu = menus[index];
         return ListTile(
+          onTap: () {
+            debugPrint('tapped: ${menu.name}');
+          },
           title: Text(menu.name),
           subtitle: Text(menu.createdAt.toIso8601String()),
         );
