37309adeeebc7761190ff2ec08481b9f28ff912b
Pregum <pregum.y@gmail.com>
2023-12-05 06:42:00 +0900

    wip: 選択した食材の特定原材料表示機能の実装途中

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:supabase_intermediate_table_pracetice_flutter/allergen.dart';
 import 'package:supabase_intermediate_table_pracetice_flutter/allergen_util.dart';
 import 'package:supabase_intermediate_table_pracetice_flutter/menu.dart';
 import 'package:supabase_intermediate_table_pracetice_flutter/menus_util.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   build(context) {
+    final selectedMenu = useState<Menu?>(null);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Supabase Flutter'),
@@ -46,7 +49,10 @@ class HomePage extends StatelessWidget {
                       if (menus == null) {
                         return const Center(child: Text('No menus found'));
                       }
-                      return _buildMenuList(menus);
+                      return _buildMenuList(
+                        menus,
+                        onTapMenu: (menu) => selectedMenu.value = menu,
+                      );
                     },
                     future: getMenus(),
                   ),
@@ -58,22 +64,29 @@ class HomePage extends StatelessWidget {
                     thickness: 4,
                   ),
                 ),
-                Expanded(
-                  child: FutureBuilder<List<Allergen>>(
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const CircularProgressIndicator.adaptive();
-                      }
+                if (selectedMenu.value != null)
+                  Expanded(
+                    key: ValueKey(selectedMenu.value!.id),
+                    child: FutureBuilder<List<Allergen>>(
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const CircularProgressIndicator.adaptive();
+                        }
 
-                      final allergens = snapshot.data;
-                      if (allergens == null) {
-                        return const Center(child: Text('No allergens found'));
-                      }
-                      return _buildAllergenList(allergens);
-                    },
-                    future: getAllergens(),
-                  ),
-                )
+                        final allergens = snapshot.data;
+                        if (allergens == null) {
+                          return const Center(
+                              child: Text('No allergens found'));
+                        }
+                        return _buildAllergenList(allergens);
+                      },
+                      // future: getAllergens(),
+                      future: getAllergensById(selectedMenu.value!.id)
+                    ),
+                  )
+                else
+                  const Expanded(child: Center(child: Text('No menu selected')))
               ],
             ),
           ),
@@ -82,13 +95,14 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _buildMenuList(List<Menu> menus) {
+  Widget _buildMenuList(List<Menu> menus, {Function(Menu menu)? onTapMenu}) {
     return ListView.builder(
       itemBuilder: (context, index) {
         final menu = menus[index];
         return ListTile(
           onTap: () {
             debugPrint('tapped: ${menu.name}');
+            onTapMenu?.call(menu);
           },
           title: Text(menu.name),
           subtitle: Text(menu.createdAt.toIso8601String()),
--- lib/menus_util.dart
+++ lib/menus_util.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_intermediate_table_pracetice_flutter/allergen.dart';
 import 'package:supabase_intermediate_table_pracetice_flutter/main.dart';
 import 'package:supabase_intermediate_table_pracetice_flutter/menu.dart';
 
@@ -12,6 +13,25 @@ Future<List<Menu>> getMenus() async {
         return Menu.fromJson(menu);
       },
     ).toList();
+  } catch (e) {
+    debugPrint('error!: $e');
+    return [];
+  }
+}
+
+Future<List<Allergen>> getAllergensById(int menuId) async {
+  try {
+    final response = await supabase
+        .from('menus')
+        .select<List<dynamic>>('id, allergens(id, type_name, created_at)')
+        .eq('id', menuId);
+    final results = response.map(
+      (allergen) {
+        debugPrint(allergen.toString());
+        return Allergen.fromJson(allergen);
+      },
+    ).toList();
+    return results;
   } catch (e) {
     debugPrint('error!: $e');
     return [];
